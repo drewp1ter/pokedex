5e53a580eee71cdd4b870d1bfd3ededbf616e262
mufdvr1 <belora2010@yandex.ru>
2023-05-24 20:46:50 +0300

    refactor

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -16,26 +16,23 @@ interface Props {
 const pageSize = 24
 
 async function Pokemons({ searchParams }: Props) {
-  const page = Math.max(Number(searchParams.page), 1) || 1
-  const { query } = searchParams
+  const { query, page } = searchParams
   let pokemons: NamedAPIResource[] = []
+  let count = 0
 
   try {
-    const res = await api.fetchPokemons(10000)
+    const res = await api.fetchPokemons({ query, page, pageSize })
     pokemons = res.results
+    count = res.count
   } catch (e: any) {
     console.error(e.message)
   }
 
-  if (query) pokemons = pokemons.filter((pokemon) => ~pokemon.name.indexOf(query))
-  const offsetStart = (page - 1) * pageSize
-  const offsetEnd = offsetStart + pageSize
-  const numberOfPages = ((pokemons.length / pageSize) >> 0) + 1
-  const currentPokemonList = pokemons.slice(offsetStart, offsetEnd)
+  const numberOfPages = ((count / pageSize) >> 0) + 1
 
   return (
     <div className={styles.home}>
-      <PokemonList pokemons={currentPokemonList} />
+      <PokemonList pokemons={pokemons} />
       <div className={styles.pages}>
         {[...Array(numberOfPages > 5 ? 5 : numberOfPages).keys()].map((key) => (
           <Link key={key} className={styles.page} href={'/' + encodeSearchParams({ query, page: key + 1 })} />
--- src/features/home/api.ts
+++ src/features/home/api.ts
@@ -3,13 +3,22 @@ import { encodeSearchParams } from '@/utils'
 const apiPrefix = 'https://pokeapi.co/api/v2/'
 const pokeEndpoint = apiPrefix + 'pokemon/'
 
-export async function fetchPokemons(limit = 24, offset = 0): Promise<NamedAPIResourceList> {
-  const res = await fetch(pokeEndpoint + encodeSearchParams({ limit, offset }), { cache: 'force-cache' })
+export async function fetchPokemons({ query, page, pageSize = 24 }: FetchPokmonProps): Promise<FetchPokemonsResponse> {
+  page = Math.max(Number(page), 1) || 1
+  let pokemons: NamedAPIResource[] = []
+
+  const res = await fetch(pokeEndpoint + encodeSearchParams({ limit: 10000, offset: 0 }), { cache: 'force-cache' })
   if (!res.ok) {
     throw new Error(`Status ${res.status}`)
   }
 
-  return res.json()
+  const data = (await res.json()) as NamedAPIResourceList
+  pokemons = data.results
+
+  if (query) pokemons = pokemons.filter((pokemon) => ~pokemon.name.indexOf(query))
+  const offsetStart = (page - 1) * pageSize
+  const offsetEnd = offsetStart + pageSize
+  return { count: pokemons.length, results: pokemons.slice(offsetStart, offsetEnd) }
 }
 
 export async function fetchPokemon(name?: string): Promise<PokemonDTO | null> {
